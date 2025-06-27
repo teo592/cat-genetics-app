3c6143b65be1591f8c9595382f7081637b06b89c
teo592 <oldcello@naver.com>
2025-06-27 11:54:49 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import CatGeneticsUploader from './CatGeneticsUploader';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 p-6">
+      <CatGeneticsUploader />
+    </div>
+  );
+}
+
+export default App;
